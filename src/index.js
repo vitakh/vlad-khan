c588ffbe66d9ea9c9ab42a7c3aec1a876472f247
Vita Khan <vita.khan.2002@mail.ru>
2025-02-12 23:34:36 -0500

    adding links

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// 
+//import "bootstrap/dist/js/bootstrap.bundle.min.js";
 //import "katex/dist/katex.min.css";
 import './index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
